87fdc9d387a19be5e802d7a3aec4a35bcd6c69d7
Gonzalo263614 <gonzalo_ruso19@outlook.com>
2024-04-30 08:41:23 -0600

    Navbar2.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EscritorioComponent } from './escritorio/escritorio.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"escritorio",component:EscritorioComponent},
+  {path:"login",component:LoginComponent},
+  {path:"**", pathMatch:"full",redirectTo:"login"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
